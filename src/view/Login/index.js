932859952e9a931d7fbf1b522f68c3eca931bfc3
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-23 18:27:30 -0300

    Fazendo sistema de login com firebase

--- src/view/Login/index.js
+++ src/view/Login/index.js
@@ -1,12 +1,41 @@
-import React from "react";
-import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 
 import logo from "../../../assets/logo.png";
 
 import styles from "./styles";
 import Button from "../../components/Button";
 
+import firebase from "../../services/firebase";
+
 const Login = ({ navigation }) => {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+
+  async function handleSignup() {
+    if ((user, pass)) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(user, pass)
+        .then((response) => {
+          Alert.alert("Ok", "Conta logada com sucesso!");
+          navigation.navigate("Index");
+        })
+        .catch((erro) => {
+          Alert.alert("Ops", "Não foi possível logar na sua conta");
+        });
+    } else {
+      Alert.alert("Erro", "Preencher o E-mail e senha");
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Image source={logo} />
@@ -17,16 +46,18 @@ const Login = ({ navigation }) => {
           keyboardType="email-address"
           autoCapitalize="none"
           placeholder="E-mail"
+          onChangeText={(text) => setUser(text)}
         />
         <TextInput
           name="user"
           style={styles.input}
-          keyboardType="email-address"
           autoCapitalize="none"
-          placeholder="E-mail"
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(text) => setPass(text)}
         />
       </View>
-      <Button style="outline" onPress={() => navigation.navigate("Index")}>
+      <Button style="outline" onPress={handleSignup}>
         <Text style={styles.text_primary}>ENTRAR</Text>
       </Button>
 
